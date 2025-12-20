c5c76becb87abc7bf3132c3caeea853137dc7c3e
mu93100 <muriel.ehlinger@lepoles.org>
2025-12-20 16:27:29 +0100

    transfert

--- zest (1).sql
+++ zest (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mer. 17 déc. 2025 à 15:53
+-- Généré le : sam. 20 déc. 2025 à 15:18
 -- Version du serveur : 9.1.0
 -- Version de PHP : 8.3.14
 
@@ -43,14 +43,54 @@ CREATE TABLE IF NOT EXISTS `adhesion` (
   KEY `IDX_C50CA65A7A45358C` (`groupe_id`),
   KEY `IDX_C50CA65ABBAAE0A4` (`montant_adhesion_id`),
   KEY `IDX_C50CA65AF965414C` (`saison_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `adhesion`
 --
 
 INSERT INTO `adhesion` (`id`, `date_adhesion`, `attentes_texte`, `competences_texte`, `paiement`, `user_id`, `groupe_id`, `montant_adhesion_id`, `saison_id`) VALUES
-(1, '2025-12-17 15:52:08', NULL, NULL, 0, 7, 7, 2, 2);
+(1, '2025-12-17 15:52:08', NULL, NULL, 0, 7, 7, 2, 2),
+(2, '2025-12-18 09:08:15', NULL, NULL, 0, 1, 7, 1, 2),
+(3, '2025-12-18 09:09:44', NULL, NULL, 0, 1, 11, 1, 2),
+(4, '2025-12-18 12:24:12', NULL, NULL, 0, 1, 12, 2, 2),
+(5, '2025-12-18 12:42:55', 'tatatatatatatatata', 'tatatatatatatatata', 0, 1, 7, 1, 2),
+(6, '2025-12-18 13:30:59', 'gfgfgfgfgfgfgfg', 'gfgfgfgfgfgfgfg', 0, 1, 12, 1, 2),
+(7, '2025-12-18 13:32:08', NULL, NULL, 0, 1, 11, 1, 2),
+(8, '2025-12-18 14:17:43', 'mmmmmmmmmmmmmmmmmmmmmmmmmmmm', 'mmmmmmmmmmmmmmmmmmmmmmmmmmmm', 0, 1, 12, 1, 2),
+(9, '2025-12-18 14:52:36', 'vbvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv', 'vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv', 0, 1, 7, 3, 2),
+(10, '2025-12-18 14:52:44', 'vbvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv', 'vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv', 0, 1, 7, 3, 2),
+(11, '2025-12-18 15:15:03', 'vbvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv', 'vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv', 0, 1, 7, 3, 2),
+(12, '2025-12-18 15:17:47', NULL, NULL, 0, 1, 12, 2, 2),
+(13, '2025-12-19 09:13:42', NULL, NULL, 0, 1, 7, 1, 2),
+(14, '2025-12-19 09:19:06', NULL, NULL, 0, 1, 12, 1, 2),
+(15, '2025-12-19 09:20:03', NULL, NULL, 0, 1, 7, 2, 2),
+(16, '2025-12-19 09:49:48', NULL, NULL, 0, 8, 7, 2, 2),
+(17, '2025-12-19 10:48:26', NULL, NULL, 0, 8, 1, 1, 2),
+(18, '2025-12-19 10:56:18', NULL, NULL, 0, 8, 9, 2, 2),
+(19, '2025-12-19 11:14:23', NULL, NULL, 0, 8, 13, 2, 2),
+(20, '2025-12-19 11:15:44', NULL, NULL, 0, 8, 13, 2, 2),
+(21, '2025-12-19 11:17:30', NULL, NULL, 0, 8, 13, 1, 2),
+(22, '2025-12-19 11:30:06', NULL, NULL, 0, 8, 7, 3, 2),
+(23, '2025-12-19 11:36:12', NULL, NULL, 0, 8, 7, 3, 2),
+(24, '2025-12-19 11:37:38', NULL, NULL, 0, 8, 7, 3, 2),
+(25, '2025-12-19 12:13:06', NULL, NULL, 0, 8, 7, 3, 2),
+(26, '2025-12-19 12:13:58', NULL, NULL, 0, 8, 7, 3, 2),
+(27, '2025-12-19 12:16:19', NULL, NULL, 0, 8, 7, 2, 2),
+(28, '2025-12-19 12:20:12', NULL, NULL, 0, 8, 7, 2, 2),
+(29, '2025-12-19 12:24:17', NULL, NULL, 0, 8, 7, 2, 2),
+(30, '2025-12-19 12:26:56', NULL, NULL, 0, 8, 7, 3, 2),
+(31, '2025-12-19 12:28:53', NULL, NULL, 0, 8, 7, 2, 2),
+(32, '2025-12-19 12:41:22', NULL, NULL, 0, 8, 7, 3, 2),
+(33, '2025-12-19 12:41:59', NULL, NULL, 0, 8, 7, 3, 2),
+(34, '2025-12-19 12:50:03', NULL, NULL, 0, 8, 7, 2, 2),
+(35, '2025-12-19 13:05:24', NULL, NULL, 0, 8, 7, 1, 2),
+(36, '2025-12-19 13:25:40', NULL, NULL, 0, 8, 7, 1, 2),
+(37, '2025-12-19 13:51:55', NULL, NULL, 0, 8, 7, 1, 2),
+(38, '2025-12-19 13:58:06', NULL, NULL, 0, 8, 7, 1, 2),
+(39, '2025-12-19 14:00:26', NULL, NULL, 0, 8, 7, 1, 2),
+(40, '2025-12-19 14:02:10', NULL, NULL, 0, 8, 7, 2, 2),
+(41, '2025-12-19 15:12:16', 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb', 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb', 0, 9, 9, 2, 2);
 
 -- --------------------------------------------------------
 
@@ -65,7 +105,21 @@ CREATE TABLE IF NOT EXISTS `adhesion_dispo` (
   PRIMARY KEY (`adhesion_id`,`dispo_id`),
   KEY `IDX_4DC30B07F68139D7` (`adhesion_id`),
   KEY `IDX_4DC30B07A18C1CC9` (`dispo_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `adhesion_dispo`
+--
+
+INSERT INTO `adhesion_dispo` (`adhesion_id`, `dispo_id`) VALUES
+(5, 1),
+(5, 2),
+(6, 1),
+(8, 1),
+(9, 1),
+(10, 1),
+(11, 1),
+(41, 1);
 
 -- --------------------------------------------------------
 
@@ -82,6 +136,23 @@ CREATE TABLE IF NOT EXISTS `adhesion_motivation` (
   KEY `IDX_690EC6E48EDBCD4E` (`motivation_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Déchargement des données de la table `adhesion_motivation`
+--
+
+INSERT INTO `adhesion_motivation` (`adhesion_id`, `motivation_id`) VALUES
+(5, 3),
+(8, 1),
+(8, 2),
+(9, 1),
+(10, 1),
+(11, 1),
+(36, 2),
+(36, 3),
+(36, 4),
+(41, 1),
+(41, 2);
+
 -- --------------------------------------------------------
 
 --
@@ -96,6 +167,25 @@ CREATE TABLE IF NOT EXISTS `adhesion_pole` (
   KEY `IDX_5717926F68139D7` (`adhesion_id`),
   KEY `IDX_5717926419C3385` (`pole_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `adhesion_pole`
+--
+
+INSERT INTO `adhesion_pole` (`adhesion_id`, `pole_id`) VALUES
+(5, 1),
+(5, 2),
+(6, 1),
+(6, 2),
+(6, 3),
+(9, 1),
+(9, 6),
+(10, 1),
+(10, 6),
+(11, 1),
+(11, 6),
+(41, 2),
+(41, 4);
 
 -- --------------------------------------------------------
 
@@ -121,7 +211,7 @@ CREATE TABLE IF NOT EXISTS `dispo` (
   `id` int NOT NULL AUTO_INCREMENT,
   `libelle` varchar(200) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `dispo`
@@ -186,7 +276,15 @@ INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_
 ('DoctrineMigrations\\Version20251216112512', '2025-12-16 11:25:26', 204),
 ('DoctrineMigrations\\Version20251216143813', '2025-12-16 14:42:48', 196),
 ('DoctrineMigrations\\Version20251217114322', '2025-12-17 11:50:08', 200),
-('DoctrineMigrations\\Version20251217115042', '2025-12-17 11:50:46', 44);
+('DoctrineMigrations\\Version20251217115042', '2025-12-17 11:50:46', 44),
+('DoctrineMigrations\\Version20251218133611', '2025-12-18 13:36:35', 126),
+('DoctrineMigrations\\Version20251218134705', '2025-12-18 13:47:10', 63),
+('DoctrineMigrations\\Version20251218141559', '2025-12-18 14:16:37', 65),
+('DoctrineMigrations\\Version20251218142435', '2025-12-18 14:24:42', 66),
+('DoctrineMigrations\\Version20251218143124', '2025-12-18 14:31:31', 59),
+('DoctrineMigrations\\Version20251218143723', '2025-12-18 14:37:25', 52),
+('DoctrineMigrations\\Version20251218143918', '2025-12-18 14:39:24', 63),
+('DoctrineMigrations\\Version20251218145123', '2025-12-18 14:51:34', 173);
 
 -- --------------------------------------------------------
 
@@ -204,7 +302,7 @@ CREATE TABLE IF NOT EXISTS `groupe` (
   `adresse_distrib` varchar(255) DEFAULT NULL,
   `ville` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `groupe`
@@ -214,10 +312,13 @@ INSERT INTO `groupe` (`id`, `nom`, `is_referent`, `is_open`, `date_creation`, `a
 (1, 'yoyo', 0, 1, '0000-00-00 00:00:00', NULL, 'mont'),
 (2, 'rrrrr', 0, 0, '0000-00-00 00:00:00', NULL, 'tata'),
 (3, 'mmm', 0, 0, '2025-12-12 10:33:50', NULL, 'montreuil'),
-(7, 'BEFANA', 0, 0, '2010-12-16 13:05:00', 'plateau', 'Bagnolet'),
-(8, '37 et +', 0, 1, '2020-02-16 13:05:00', '37 rue de Vincennes', 'Montreuil'),
+(7, 'BEFANA', 0, 0, '2010-12-16 13:05:00', 'plateau plateau plateau', 'bagnolettteOO'),
+(8, '37 et +', 0, 0, '2020-02-16 13:05:00', '37 rue de Vincennes', 'Montreuil'),
 (9, 'ALEP', 0, 1, '2011-01-20 09:30:00', '7 rue Alexis Lepère', 'Montreuil'),
-(10, 'hh', 0, 0, '2025-12-17 10:15:09', NULL, 'montreuil');
+(10, 'hh', 0, 0, '2025-12-17 10:15:09', NULL, 'montreuil'),
+(11, 'titine', 0, 0, '2025-12-18 09:09:44', '51 rue michelet', 'bagnolet'),
+(12, 'API', 0, 0, '2025-12-18 12:24:16', '166 rue edouard branly', 'montreuil'),
+(13, 'kkkkkkkkkkkkkkkkk', 0, 0, '2025-12-19 11:14:23', '166 rue edouard branly', 'montreuil');
 
 -- --------------------------------------------------------
 
@@ -511,17 +612,19 @@ CREATE TABLE IF NOT EXISTS `user` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `UNIQ_IDENTIFIER_EMAIL` (`email`),
   KEY `IDX_8D93D6497A45358C` (`groupe_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `user`
 --
 
 INSERT INTO `user` (`id`, `nom`, `prenom`, `email`, `password`, `telephone`, `adresse`, `code_postal`, `ville`, `date_de_naissance`, `composition_foyer`, `nombre_enfants`, `roles`, `groupe_id`, `is_referent`) VALUES
-(1, 'ehl', 'muri', 'murielehlinger@gmail.com', '$2y$13$diIjwGFCdgVEzYNI5TxA9.33Do97Xhe1MRW8Oehr/p1ZjL6CpSmd.', '0686420135', NULL, '93100', 'montreuil', NULL, NULL, NULL, '[]', 1, 0),
+(1, 'ehl', 'muri', 'murielehlinger@gmail.com', '$2y$13$diIjwGFCdgVEzYNI5TxA9.33Do97Xhe1MRW8Oehr/p1ZjL6CpSmd.', '0686420135', NULL, '93100', 'montreuil', NULL, NULL, NULL, '[]', 7, 1),
 (2, 'hhh', 'hhhref', 'muriel@gmail.com', '$2y$13$25OSDzF/FTvy2uqHGpMzeeDcWzcQk8lJx.XBCzEfC8x5/EBf5wf/W', '0686420135', NULL, '93100', 'montreuil', NULL, NULL, NULL, '[\"ROLE_USER\", \"ROLE_ADMIN\"]', 2, 0),
 (3, 'chromosores', 'chromosores', 'ehlinger@gmail.com', '$2y$13$FWt58qx.NhKEMfeHvCH3I.OHi9I9.H/nhlcIMc3tBquFk0mzXYclK', '0686420135', NULL, '93100', 'montreuil', NULL, NULL, NULL, '[\"ROLE_ADMIN\"]', 2, 0),
-(7, 'hh', 'hh', 'murcccccciel.ehlinger@lepoles.org', '$2y$13$v7hfaOOt8xpzzOS571jcv.eFQnUhvhGRyW63Yf0aM7aJnVL8uTQ/.', '0686420135', NULL, '12457', 'montreuil', NULL, NULL, NULL, '[]', 7, 0);
+(7, 'hh', 'hh', 'murcccccciel.ehlinger@lepoles.org', '$2y$13$v7hfaOOt8xpzzOS571jcv.eFQnUhvhGRyW63Yf0aM7aJnVL8uTQ/.', '0686420135', NULL, '12457', 'montreuil', NULL, NULL, NULL, '[]', 7, 0),
+(8, 'essaiMDP mdpessai', 'essai', 'essai@gmail.com', '$2y$13$2e7OzuNQbsgMO5lWNfPgpe1PMhPs2IUZlaHzmAZa4.KZeEFZ55Xei', '0686420135', '134 rue saint 93100 Montreuil', '93100', 'montreuil', NULL, 3, 2, '[]', 7, 0),
+(9, 'facundo', 'facundo', 'facundor@lepoles.org', '$2y$13$e40YZ9AOOUq5vXo/MJshCu0bda1SfvZvC9n1IFA3eFCge/67dWdva', '0686420135', '134 rue saint 93100 Montreuil', '93100', 'montreuil', '2025-12-24', 2, 1, '[]', 9, 1);
 
 -- --------------------------------------------------------
 
@@ -552,18 +655,25 @@ ALTER TABLE `adhesion`
   ADD CONSTRAINT `FK_C50CA65AF965414C` FOREIGN KEY (`saison_id`) REFERENCES `saison` (`id`);
 
 --
+-- Contraintes pour la table `adhesion_dispo`
+--
+ALTER TABLE `adhesion_dispo`
+  ADD CONSTRAINT `FK_adhesion` FOREIGN KEY (`adhesion_id`) REFERENCES `adhesion` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_dispo` FOREIGN KEY (`dispo_id`) REFERENCES `dispo` (`id`) ON DELETE CASCADE;
+
+--
 -- Contraintes pour la table `adhesion_motivation`
 --
 ALTER TABLE `adhesion_motivation`
-  ADD CONSTRAINT `FK_690EC6E48EDBCD4E` FOREIGN KEY (`motivation_id`) REFERENCES `motivation` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `FK_690EC6E4F68139D7` FOREIGN KEY (`adhesion_id`) REFERENCES `adhesion` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `FK_adhesion_motivation` FOREIGN KEY (`adhesion_id`) REFERENCES `adhesion` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_motivation` FOREIGN KEY (`motivation_id`) REFERENCES `motivation` (`id`) ON DELETE CASCADE;
 
 --
 -- Contraintes pour la table `adhesion_pole`
 --
 ALTER TABLE `adhesion_pole`
-  ADD CONSTRAINT `FK_5717926419C3385` FOREIGN KEY (`pole_id`) REFERENCES `pole` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `FK_5717926F68139D7` FOREIGN KEY (`adhesion_id`) REFERENCES `adhesion` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `FK_adhesion_pole` FOREIGN KEY (`adhesion_id`) REFERENCES `adhesion` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_pole` FOREIGN KEY (`pole_id`) REFERENCES `pole` (`id`) ON DELETE CASCADE;
 
 --
 -- Contraintes pour la table `photos`
